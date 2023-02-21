1c4f9a4e2c79b1af10ed9d7241e5c736b6d0afbc
F. Camuncoli <f.camuncoli@gmail.com>
2023-02-21 13:02:33 -0500

    added search button to search form and updated CSS

--- src/Search.js
+++ src/Search.js
@@ -5,15 +5,22 @@ export default function Search() {
   return (
     <div className="Search">
       <form>
-        <div className="mb-3 search-form shadow">
-          <input
-            type="text"
-            className="form-control"
-            aria-describedby="emailHelp"
-            placeholder="Search City"
-          />
+        <div className="row">
+          <div className="col-8 search-form">
+            <input
+              type="search"
+              className="form-control"
+              aria-describedby="emailHelp"
+              placeholder="Search City"
+            />
+          </div>
+
+          <div className=" col-3">
+            <input type="submit" value="search" className="search-button" />
+          </div>
         </div>
       </form>
+
       <div className="button">
         <button className="Location-button">Current Location</button>
       </div>
